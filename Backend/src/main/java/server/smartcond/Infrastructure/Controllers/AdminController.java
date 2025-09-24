089df8d09956015e8675fc4a81c1007851022393
nestorduqueduque <nestorduqueduque@gmail.com>
2025-09-24 11:02:33 -0500

    add swagger

--- Backend/src/main/java/server/smartcond/Infrastructure/Controllers/AdminController.java
+++ Backend/src/main/java/server/smartcond/Infrastructure/Controllers/AdminController.java
@@ -1,6 +1,8 @@
 package server.smartcond.Infrastructure.Controllers;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,47 +17,50 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
+@Tag(name ="Admin Endpoints")
 public class AdminController {
 
     @Autowired
     private IAdminService adminService;
 
     //Celador Endpoints
-    //Find all Celadors
+
+
+    @Operation(summary = "get all the celadors")
     @GetMapping("/find-all-celador")
     public ResponseEntity<List<CeladorResponseDTO>> findAllCeladors(){
         return new ResponseEntity<>(this.adminService.findAllCeladors(), HttpStatus.OK);
     }
 
-    @GetMapping("/get")
-    public  String hello(){
-        return "hello World";
-    }
 
-    //Find Celador by id
-    @GetMapping("/find-celador/{id}")
+    @Operation(summary = "Get Celadors by Id")
     public ResponseEntity<CeladorResponseDTO> findById(@PathVariable Long id){
         return new ResponseEntity<>(this.adminService.finById(id), HttpStatus.OK);
     }
 
-    //Update Celador
+
+    @Operation(summary = "Update Celadors by Id")
     @PutMapping("/update-celador/{id}")
     public ResponseEntity<CeladorResponseDTO> updateCelador(@RequestBody CeladorRequestDTO celadorRequestDTO, @PathVariable Long id){
         return new ResponseEntity<>(this.adminService.updateCelador(celadorRequestDTO, id), HttpStatus.CREATED);
     }
-    //Create User Celador
+
+    @Operation(summary = "Create Celador")
     @PostMapping("/create-celador")
         public ResponseEntity<CeladorResponseDTO> createCelador(@RequestBody CeladorRequestDTO celadorRequestDTO){
         return new ResponseEntity<>(this.adminService.createCelador(celadorRequestDTO), HttpStatus.CREATED);
         }
 
 
-    //Resident Endpoints
+
+    @Operation(summary = "Create Resident")
     @PostMapping("/create-resident")
     public ResponseEntity<ResidentResponseDTO> createResident(@RequestBody ResidentRequestDTO residentRequestDTO){
         return new ResponseEntity<>(this.adminService.createResident(residentRequestDTO), HttpStatus.CREATED);
     }
 
+
+    @Operation(summary = "Get all Residents")
     @GetMapping("/find-all-resident")
     public ResponseEntity<List<ResidentResponseDTO>> findAllResidents(){
         return new ResponseEntity<>(this.adminService.findAllResidents(), HttpStatus.OK);
